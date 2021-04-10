349d41125837b135d448e980fae52c7950d8b820
towfeeqfayaz11 <towfeeqpandith@gmail.com>
2021-04-10 18:40:17 +0900

    first js commit

---
+++ constants.js
@@ -0,0 +1,14 @@
+// variable are defined using let keyword, their values can be chnaged anytime in the code
+let fname = "jack";
+fname = "mike";
+console.log(fname);
+
+// constants are defined using const keyword, once defined, their value cannot be changed in the code
+const phoneno = 12346789;
+//phoneno = 22;  constants cannot be reassigned
+console.log(phoneno);
+
+const addr = "jaya nagar";
+console.log(addr)
+//addr = 'ahmedabad' reassignment not possible for constants 
+console.log(addr)
---
+++ variables.js
@@ -0,0 +1,4 @@
+let name = "hello guys";    // let keyword is used to define variables
+let fname = 'jack', lastName;   // mike is still undefined
+console.log(name);          // each statement is ended with a semi colon (;)
+console.log(fname,lastName)
